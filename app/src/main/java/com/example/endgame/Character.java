787f16fbf190aa9622eb9c9b0c4c6d3e38ed95e6
Sheriff Ameen <sheriffameen1@gmail.com>
2019-03-26 22:36:34 -0400

    initial commit

---
+++ app/src/main/java/com/example/endgame/Character.java
@@ -0,0 +1,44 @@
+package com.example.endgame;
+
+public class Character {
+    private String name;
+    private String quote;
+    private String gender;
+    private String height;
+    private String weight;
+    private String history;
+    private String occupation;
+    private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public String getHistory() {
+        return history;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
---
+++ app/src/main/java/com/example/endgame/CharacterResponse.java
@@ -0,0 +1,11 @@
+package com.example.endgame;
+
+import java.util.List;
+
+public class CharacterResponse {
+    private List<Character> characters;
+
+    public List<Character> getCharacters() {
+        return characters;
+    }
+}
